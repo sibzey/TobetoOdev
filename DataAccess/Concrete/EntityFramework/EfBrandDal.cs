b6cc12de24b4d22bdb50f83ca28663d51336714b
Sibel Öztürk <141643764+sibzey@users.noreply.github.com>
2024-02-20 19:55:57 +0300

    a

---
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+internal class EfBrandDal : EfEntityRepositoryBase<Brand, int,RentACarContext>,IBrandDal
+{
+    public EfBrandDal(RentACarContext context) : base(context)
+    {
+    }
+}
+   
+}
